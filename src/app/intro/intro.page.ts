751b739d510bd57b3f2df932893faac02a4ae035
Renato De la Rosa <rdelarosa@sodimac.com.pe>
2021-01-03 18:17:16 -0500

    Agregando componente Slides

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage {
+  constructor(private router: Router) {}
+  slideOptions = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400,
+  };
+
+  slides = [
+    {
+      logoUrl: 'assets/images/logo-h.png',
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsum temporibus nostrum sit totam reiciendis, quia libero nisi nihil magni nemo laborum officiis iure in sed aut amet excepturi obcaecati!',
+      iconName: 'camera',
+    },
+    {
+      logoUrl: 'assets/images/logo-h.png',
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsum temporibus nostrum sit totam reiciendis, quia libero nisi nihil magni nemo laborum officiis iure in sed aut amet excepturi obcaecati!',
+      iconName: 'airplane',
+    },
+    {
+      logoUrl: 'assets/images/logo-h.png',
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsum temporibus nostrum sit totam reiciendis, quia libero nisi nihil magni nemo laborum officiis iure in sed aut amet excepturi obcaecati!',
+      iconName: 'restaurant',
+    },
+  ];
+
+  closeSlides() {
+    this.router.navigateByUrl('/home');
+  }
+}
