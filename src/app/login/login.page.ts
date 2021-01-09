d117bc8e39cf7e6f5b217fc675261a6332c3c19f
Renato De la Rosa <rdelarosa@sodimac.com.pe>
2021-01-09 10:48:56 -0500

    Formulario Reactivo y Validaciones

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'El email es requerido.' },
+      { type: 'pattern', message: 'El email no es válido.' },
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es requerida.' },
+      {
+        type: 'minlength',
+        message: 'La contraseña debe tener un minino de 5 caracteres.',
+      },
+    ],
+  };
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ])
+      ),
+      password: new FormControl(
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)])
+      ),
+    });
+  }
+
+  ngOnInit() {}
+}
